54c33b80b949c3b67ec9273b10967a03305340c5
Adrien <adrien.biz@gmail.com>
2023-01-15 13:24:19 +0400

    Format index.html with prettier

--- fibonnaci.js
+++ fibonnaci.js
@@ -14,13 +14,13 @@ function fibs(n) {
 
   return arr;
 }
-  
+
 function fibsRec(n) {
   if (n === 1) return [0];
   if (n === 2) return [0, 1];
   return [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
 }
-  
+
 console.log(fibs(8));
 console.log(fibsRec(8));
 
